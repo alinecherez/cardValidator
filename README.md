f8024c91dd1047608785fb2b25b8678a3d354799
Aline Cherez <alinecherez@gmail.com>
2019-05-02 18:41:19 -0300

    Biblioteca para validação de cartão de crédito

---
+++ README.md
@@ -0,0 +1,15 @@
+# Validador de cartões de crédito v.1.0.0
+Esta biblioteca se destina à verificação da sequência de números de cartões de crédito para uso em aplicações web.
+Na versão atual, através do Algoritmo de Luhn é capaz de validar os números de um cartão de crédito, evitando erros de digitação.
+
+## Como instalar:
+$  npm install cardvalidatorac
+
+## Como utilizar:
+> const cardValidator = require('../lib/index');
+> console.log(cardValidator(4716421870804403))
+> // returns "true"
+
+##versão 1.0.0 (released)
+* funcionalidade: validação da sequência de números de um cartão de crédito.
+* aceita somente cartões de 16 dígitos.
---
+++ test/index.spec.js
@@ -0,0 +1,27 @@
+/*eslint-disable */
+
+const chai = require('chai');
+const expect = chai.expect;
+const cardValidator = require('../lib/index');
+
+describe('card lib', () => {
+
+	it('Deveria retornar true para o cartão 4716421870804403', () => {
+		expect(cardValidator.cardValidator('4716421870804403')).to.equal(true);
+	});
+	
+	it('Deveria retornar false para o cartão 3928471039285700', () => {
+		expect(cardValidator.cardValidator('3928471039285700')).to.equal(false);
+	});
+
+	it('Deveria retornar false para o cartão 39284710392857', () => {
+		expect(cardValidator.cardValidator('39284710392857')).to.equal(false);
+	});	
+
+	it('Deveria retornar false para o cartão abcdefghijklmnop', () => {
+		expect(cardValidator.cardValidator('abcdefghijklmnop')).to.equal(false);
+	});
+
+	
+
+}); 
